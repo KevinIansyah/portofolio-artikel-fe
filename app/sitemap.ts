f72cf4e63eee844ef2378223c58de5f859a280e2
KevinIansyah <keviniansyah04@gmail.com>
2026-02-19 13:39:45 +0700

    change: sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,32 @@
 import { MetadataRoute } from "next";
+import { apiServer } from "@/lib/api/server";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [{ url: "https://keviniansyah.site" }, { url: "https://keviniansyah.site/projects" }, { url: "https://keviniansyah.site/articles" }];
+const baseUrl = "https://keviniansyah.site";
+
+type Item = {
+  slug: string;
+  updated_at: string;
+};
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const projects = await apiServer.get<Item[]>("/api/projects");
+  const articles = await apiServer.get<Item[]>("/api/articles");
+
+  const urls: MetadataRoute.Sitemap = [{ url: baseUrl }, { url: `${baseUrl}/projects` }, { url: `${baseUrl}/articles` }];
+
+  projects.forEach((project) => {
+    urls.push({
+      url: `${baseUrl}/projects/${project.slug}`,
+      lastModified: new Date(project.updated_at),
+    });
+  });
+
+  articles.forEach((article) => {
+    urls.push({
+      url: `${baseUrl}/articles/${article.slug}`,
+      lastModified: new Date(article.updated_at),
+    });
+  });
+
+  return urls;
 }
